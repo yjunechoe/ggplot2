bb7af97b18ada1acba323e8c7e579f495218828d
yjunechoe <jchoe001@gmail.com>
2025-05-15 15:53:34 -0400

    spell out identity()

--- R/scale-.R
+++ R/scale-.R
@@ -959,7 +959,9 @@ ScaleDiscrete <- ggproto("ScaleDiscrete", Scale,
     self$range$train(x, drop = self$drop, na.rm = !self$na.translate)
   },
 
-  transform = identity,
+  transform = function(self, x) {
+    x
+  },
 
   map = function(self, x, limits = self$get_limits()) {
     limits <- limits[!is.na(limits)]
